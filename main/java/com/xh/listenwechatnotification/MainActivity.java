4d74b1ac89dc463d0ff5f30439c8553dcb351e87
Dennis-Humming <96933126+Dennis-Humming@users.noreply.github.com>
2022-01-03 22:37:10 +0800

    Add files via upload

---
+++ main/java/com/xh/listenwechatnotification/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lanboxhB18031226.listenwechatnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
